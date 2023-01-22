9d0fec082f78373576128ec72ba72ac3bef7b3aa
ali-farhad <alifarhad557@gmail.com>
2023-01-21 22:50:39 -0800

    finished landing page

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    
+    fontFamily: {
+      'sans': ['Roboto', 'sans-serif'],
+    },
+    
+    extend: {
+      colors: {
+        'bluish': '#006cff',
+        'footerDarkish': 'rgb(25, 43, 55)',
+        'footerDark': '#001522',
+        'creamish': 'rgb(240, 235, 235)',
+        'whitish': '#f5f5f5'
+      },
+    },
+  },
+  plugins: [],
+}
